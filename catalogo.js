ec5d5a0eb77fc785f3bd02153addad11a40b0559
Gianluca Mariano <giangimar04@gmail.com>
2023-12-11 15:14:14 +0100

    Ver 3.0

--- catalogo.js
+++ catalogo.js
@@ -1,31 +1,48 @@
 
+let prod_attivo;
 
 function layerAcquista(id) {
     
-    
-    
-    if (document.getElementById(`layer-image-prod-${id}`).style.zIndex == "-1") {
+    if (prod_attivo == null) {
         
         
-        
-        document.getElementById(`layer-image-prod-${id}`).style.zIndex = "auto";
-        document.getElementById(`layer-prod-${id}`).style.zIndex = "auto";
-        document.getElementById(`button-acq-${id}`).style.opacity = "0";
+        if (document.getElementById(`layer-image-prod-${id}`).style.zIndex == "-1") {
+            
+            
+            
+            document.getElementById(`layer-image-prod-${id}`).style.zIndex = "auto";
+            document.getElementById(`layer-prod-${id}`).style.zIndex = "auto";
+            document.getElementById(`button-acq-${id}`).style.opacity = "0";
+            
+            
+            
+            
+        } else {
+            
+            
+            document.getElementById(`layer-image-prod-${id}`).style.zIndex = "-1";
+            document.getElementById(`layer-prod-${id}`).style.zIndex = "-1";
+            document.getElementById(`button-acq-${id}`).style.opacity = "1";
+            
+            prod_attivo = id;
+            
+            
+        }
 
-        
-
-        
     } else {
-        
-        
-        document.getElementById(`layer-image-prod-${id}`).style.zIndex = "-1";
-        document.getElementById(`layer-prod-${id}`).style.zIndex = "-1";
-        document.getElementById(`button-acq-${id}`).style.opacity = "1";
 
-       
-        
+            document.getElementById(`layer-image-prod-${prod_attivo}`).style.zIndex = "auto";
+            document.getElementById(`layer-prod-${prod_attivo}`).style.zIndex = "auto";
+            document.getElementById(`button-acq-${prod_attivo}`).style.opacity = "0";
+
+            document.getElementById(`layer-image-prod-${id}`).style.zIndex = "-1";
+            document.getElementById(`layer-prod-${id}`).style.zIndex = "-1";
+            document.getElementById(`button-acq-${id}`).style.opacity = "1";
+            
+            prod_attivo = id;
+           
+
     }
-    
     
     
     
