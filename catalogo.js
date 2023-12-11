7f2c5f3f025aa23cdd957d8c2d28055703fd1cf6
Gianluca Mariano <giangimar04@gmail.com>
2023-12-11 15:47:41 +0100

    Ver 3.0

--- catalogo.js
+++ catalogo.js
@@ -1,5 +1,6 @@
 
 let prod_attivo;
+let open_menu;
 
 function layerAcquista(id) {
     
@@ -28,20 +29,20 @@ function layerAcquista(id) {
             
             
         }
-
+        
     } else {
-
-            document.getElementById(`layer-image-prod-${prod_attivo}`).style.zIndex = "auto";
-            document.getElementById(`layer-prod-${prod_attivo}`).style.zIndex = "auto";
-            document.getElementById(`button-acq-${prod_attivo}`).style.opacity = "0";
-
-            document.getElementById(`layer-image-prod-${id}`).style.zIndex = "-1";
-            document.getElementById(`layer-prod-${id}`).style.zIndex = "-1";
-            document.getElementById(`button-acq-${id}`).style.opacity = "1";
-            
-            prod_attivo = id;
-           
-
+        
+        document.getElementById(`layer-image-prod-${prod_attivo}`).style.zIndex = "auto";
+        document.getElementById(`layer-prod-${prod_attivo}`).style.zIndex = "auto";
+        document.getElementById(`button-acq-${prod_attivo}`).style.opacity = "0";
+        
+        document.getElementById(`layer-image-prod-${id}`).style.zIndex = "-1";
+        document.getElementById(`layer-prod-${id}`).style.zIndex = "-1";
+        document.getElementById(`button-acq-${id}`).style.opacity = "1";
+        
+        prod_attivo = id;
+        
+        
     }
     
     
@@ -49,4 +50,25 @@ function layerAcquista(id) {
     
     
     
+}
+
+
+
+function menu() {
+    
+    if (open_menu == null || open_menu == false) {
+        
+        document.getElementById("menu_card").style.display = "flex";
+        
+        open_menu = true;
+        
+    }
+    else {
+
+        
+        document.getElementById("menu_card").style.display = "none";
+
+        open_menu = false;
+
+    }
 }
